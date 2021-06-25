924caa1cfbdbc776998d861356597c0dfa9a6bfc
Jeong Han Lee <jeonghan.lee@gmail.com>
2021-06-25 14:43:05 -0700

    remove out-of-range exit code

--- scripts/build_epics.bash
+++ scripts/build_epics.bash
@@ -21,17 +21,17 @@ fi
 
 pushd "${SC_TOP}/.." || exit
 echo "INSTALL_LOCATION:=${INSTALL_LOCATION}" > configure/CONFIG_SITE.local 
-make -s init || exit -1
-make -s conf || exit -1
-make -s patch || exit -1
+make -s init || exit
+make -s conf || exit
+make -s patch || exit
 epics_path=$(make -s print-INSTALL_LOCATION_EPICS)
 base_path=$(make -s print-INSTALL_LOCATION_BASE)
 modules_path=$(make -s print-INSTALL_LOCATION_MODS)
 epics_vers=$(make -s  print-PATH_NAME_EPICSVERS)
 symlink_epics_path="${INSTALL_LOCATION}/epics/R${epics_vers}"
-make -s build || exit -1
-make -s install || exit -1
-make -s symlinks || exit -1
+make -s build || exit
+make -s install || exit
+make -s symlinks || exit
 mkdir -p "${symlink_epics_path}"
 pushd "${symlink_epics_path}" || exit
 cp -f "${epics_path}/setEpicsEnv.bash" .
